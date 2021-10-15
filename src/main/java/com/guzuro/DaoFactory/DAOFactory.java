afb783e47ae1596ef4ea49cd94d865b273fe2cf5
guzuro <guzurogmn@gmail.com>
2021-10-16 01:49:55 +0300

    factory upd

--- src/main/java/com/guzuro/DaoFactory/DAOFactory.java
+++ src/main/java/com/guzuro/DaoFactory/DAOFactory.java
@@ -1,12 +1,15 @@
 package com.guzuro.DaoFactory;
 
+import com.guzuro.Commentary.CommentaryDao;
 import com.guzuro.Todo.TodoDao;
+import io.vertx.core.Vertx;
 
 public abstract class DAOFactory {
 
     public static final int POSTGRES = 1;
 
-    public abstract TodoDao getTodoDAO();
+    public abstract TodoDao getTodoDAO(Vertx vertx);
+    public abstract CommentaryDao getCommentaryDAO(Vertx vertx);
 
     public static DAOFactory getDAOFactory(int factoryType) {
         switch (factoryType) {
--- src/main/java/com/guzuro/DaoFactory/PostgresDAOFactory.java
+++ src/main/java/com/guzuro/DaoFactory/PostgresDAOFactory.java
@@ -1,5 +1,7 @@
 package com.guzuro.DaoFactory;
 
+import com.guzuro.Commentary.CommentaryDao;
+import com.guzuro.Commentary.PostgresCommentaryDAOImpl;
 import com.guzuro.Todo.PostgresTodoDaoImpl;
 import com.guzuro.Todo.TodoDao;
 import io.vertx.core.Vertx;
@@ -13,10 +15,10 @@ public class PostgresDAOFactory extends DAOFactory {
     public static SqlClient createConnection(Vertx vertx) {
         PgConnectOptions connectOptions = new PgConnectOptions()
                 .setPort(5432)
-                .setHost("********")
-                .setDatabase("*********")
-                .setUser("**********")
-                .setPassword("*************");
+                .setHost("*")
+                .setDatabase("*")
+                .setUser("*")
+                .setPassword("*");
 
         PoolOptions poolOptions = new PoolOptions()
                 .setMaxSize(5);
@@ -25,7 +27,11 @@ public class PostgresDAOFactory extends DAOFactory {
         return pgClient;
     }
 
-    public TodoDao getTodoDAO() {
-        return new PostgresTodoDaoImpl();
+    public TodoDao getTodoDAO(Vertx vertx) {
+        return new PostgresTodoDaoImpl(vertx);
+    }
+
+    public CommentaryDao getCommentaryDAO(Vertx vertx) {
+        return new PostgresCommentaryDAOImpl(vertx);
     }
 }
