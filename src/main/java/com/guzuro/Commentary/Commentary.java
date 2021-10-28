92b88ac7d0fdfbad0587b220dbc905902693135f
guzuro <guzurogmn@gmail.com>
2021-10-28 23:10:23 +0300

    Commentary dao upd

--- src/main/java/com/guzuro/Commentary/Commentary.java
+++ src/main/java/com/guzuro/Commentary/Commentary.java
@@ -1,5 +1,7 @@
 package com.guzuro.Commentary;
 
+import java.time.LocalDateTime;
+
 public class Commentary {
     private Number id;
 
@@ -7,12 +9,15 @@ public class Commentary {
 
     private Number todo_id;
 
+    private LocalDateTime created_at;
+
     public Commentary(){}
 
-    public Commentary(Number id, String text, Number todo_id) {
+    public Commentary(Number id, String text, Number todo_id, LocalDateTime created_at) {
         this.id = id;
         this.text = text;
         this.todo_id = todo_id;
+        this.created_at = created_at;
     }
 
     public String getText() {
@@ -37,5 +42,13 @@ public class Commentary {
 
     public void setTodo_id(Number todo_id) {
         this.todo_id = todo_id;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
     }
 }
--- src/main/java/com/guzuro/Commentary/PostgresCommentaryDAOImpl.java
+++ src/main/java/com/guzuro/Commentary/PostgresCommentaryDAOImpl.java
@@ -1,7 +1,5 @@
 package com.guzuro.Commentary;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.guzuro.DaoFactory.PostgresDAOFactory;
 import io.vertx.core.Vertx;
 import io.vertx.sqlclient.Row;
@@ -9,6 +7,8 @@ import io.vertx.sqlclient.RowSet;
 import io.vertx.sqlclient.SqlClient;
 import io.vertx.sqlclient.Tuple;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -27,18 +27,21 @@ public class PostgresCommentaryDAOImpl implements CommentaryDao {
         CompletableFuture<CopyOnWriteArrayList<Commentary>> future = new CompletableFuture<>();
 
         pgClient
-                .preparedQuery("SELECT id, text, todo_id FROM commentary WHERE todo_id=$1")
+                .preparedQuery("SELECT id, text, todo_id, created_at FROM commentary WHERE todo_id=$1")
                 .execute(Tuple.of(todoId), ar -> {
                     if (ar.succeeded()) {
                         RowSet<Row> result = ar.result();
-                        ObjectMapper objectMapper = new ObjectMapper();
                         for (Row row : result) {
-                            try {
-                                Commentary commentary = objectMapper.readValue(row.toJson().toString(), Commentary.class);
-                                commentaryList.add(commentary);
-                            } catch (JsonProcessingException e) {
-                                future.completeExceptionally(e.getCause());
+                            String time = row.toJson().getString("created_at");
+                            LocalDateTime created_at = null;
+                            if (time != null) {
+                                DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+                                created_at = LocalDateTime.parse(time, formatter);
                             }
+                            Number id = row.toJson().getNumber("id");
+                            Number todo_id = row.toJson().getNumber("todo_id");
+                            String text = row.toJson().getString("text");
+                            commentaryList.add(new Commentary(id, text, todo_id, created_at));
                         }
                         future.complete(commentaryList);
                     } else {
@@ -53,18 +56,20 @@ public class PostgresCommentaryDAOImpl implements CommentaryDao {
         CompletableFuture<Commentary> future = new CompletableFuture<>();
 
         pgClient
-                .preparedQuery("INSERT INTO commentary(text, todo_id) VALUES($1, $2) returning id, text, todo_id")
-                .execute(Tuple.of(commentary.getText(), commentary.getTodo_id()), ar -> {
+                .preparedQuery("INSERT INTO commentary(text, todo_id, created_at) VALUES($1, $2, $3) returning id, text, todo_id, created_at")
+                .execute(Tuple.of(commentary.getText(), commentary.getTodo_id(), commentary.getCreated_at()), ar -> {
                     if (ar.succeeded()) {
                         Row result = ar.result().iterator().next();
-                        Commentary commentFromDb = null;
-                        try {
-                            ObjectMapper objectMapper = new ObjectMapper();
-                            commentFromDb = objectMapper.readValue(result.toJson().toString(), Commentary.class);
-                            future.complete(commentFromDb);
-                        } catch (JsonProcessingException e) {
-                            future.completeExceptionally(e.getCause());
-                        }
+
+                        String time = result.toJson().getString("created_at");
+                        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+                        LocalDateTime created_at = LocalDateTime.parse(time, formatter);
+                        Number id = result.toJson().getNumber("id");
+                        Number todo_id = result.toJson().getNumber("todo_id");
+                        String text = result.toJson().getString("text");
+
+                        future.complete(new Commentary(id, text, todo_id, created_at));
                     } else {
                         future.completeExceptionally(ar.cause());
                     }
